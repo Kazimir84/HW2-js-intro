c42cb89ee71d67fd94d16dbe70fbd21652c3d9ee
Kazimir84 <kav2007@ukr.net>
2021-06-26 12:21:13 +0300

    Add space beetwen operatot else and dell space in case

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ console.log('------------- # 7');
 function getMarkInfo(condition) {
     if (condition === 10) {
         console.log('У вас максимальный балл');
-    }else{
+    } else {
         console.log ('У вас средний балл');
     };
 };
@@ -33,22 +33,22 @@ function getDayInfo(position){
         case 0:
             console.log ('Sunday');
             break;
-        case 1 :
+        case 1:
             console.log ('Monday');
             break;
-        case 2 :
+        case 2:
             console.log ('Tuesday');
             break;
-        case 3 :
+        case 3:
             console.log ('Wednesday');
             break;
-        case 4 :
+        case 4:
             console.log ('Thursday');
             break;
-        case 5 :
+        case 5:
             console.log ('Friday');
             break;
-        case 6 :
+        case 6:
             console.log ('Saturday');
             break;
         default:
